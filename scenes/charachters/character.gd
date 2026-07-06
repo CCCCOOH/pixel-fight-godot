0626bc0b9c03e04f653a92fea3b5ec2dcf7fc3fa
CCCCOOH <c3156298376@163.com>
2026-07-06 23:01:22 +0800

    制作了Barrel（木桶）

--- scenes/charachters/character.gd
+++ scenes/charachters/character.gd
@@ -6,7 +6,7 @@ extends CharacterBody2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var character_sprite: Sprite2D = $CharacterSprite
 
-enum State {IDLE, WALK}
+enum State {IDLE, WALK, ATTACK}
 
 var state = State.IDLE
 
@@ -18,24 +18,39 @@ func _process(_delta: float) -> void:
 	move_and_slide()
 
 func handle_movement() -> void:
-	if velocity.length() == 0:
-		state = State.IDLE
+	if can_walk():
+		if velocity.length() == 0:
+			state = State.IDLE
+		else:
+			state = State.WALK
 	else:
-		state = State.WALK
+		velocity = Vector2.ZERO
 
 func handle_input() -> void:
 	var direction = Input.get_vector("left", 'right', 'up', 'down')
 	velocity = direction * speed
+	if can_attack() && Input.is_action_just_pressed("attack"):
+		state = State.ATTACK
 
 func handle_animations() -> void:
 	if state == State.IDLE:
 		animation_player.play('idle')
 	elif state == State.WALK:
 		animation_player.play("walk")
+	elif state == State.ATTACK:
+		animation_player.play("punch")
 		
 func handle_sprite() -> void:
 	if velocity.x < 0:
 		character_sprite.flip_h = true
-	else:
+	elif velocity.x > 0:
 		character_sprite.flip_h = false
 		
+func can_attack() -> bool:
+	return state == State.IDLE or state == State.WALK
+	
+func can_walk() -> bool:
+	return state == State.IDLE or state == State.WALK
+
+func on_action_complete() -> void:
+	state = State.IDLE
--- scenes/stage/world.gd
+++ scenes/stage/world.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-@onready var player: CharacterBody2D = $Player
-@onready var camera: Camera2D = $Camera
+@export var player: CharacterBody2D
+@export var camera: Camera2D
 
 func _process(_delta: float) -> void:
 	if player.position.x > camera.position.x:
